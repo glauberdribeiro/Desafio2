6ccd06bc3853e24a394cee4fa2df8d00e31c7205
Glauber <glauberdribeiro@gmail.com>
2022-09-25 16:30:33 -0300

    Desafio 2
    Middleware modificado conforme os requisitos

--- src/index.js
+++ src/index.js
@@ -10,19 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(user){
+    request.user = user;
+    return next();
+  }else{
+    return response.status(404).json();
+  }
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  _countTodo = user.todos.length;
+
+  if(user.pro === true ){
+    return next();
+  }else if(user.pro === false && _countTodo < 10){
+    return next();
+  }else if(user.pro === false && _countTodo > 9){
+    return response.status(403).json({error: 'Usuário não tem plano PRO ou ja tem 10 todos'});
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+  
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: 'User not found'});
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({error: 'Id not type of uuid'});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+
+
+  if(todo){
+    request.user = user;
+    request.todo = todo;
+    return next();
+  }else{
+    return response.status(404).json({error: 'Todo não pertence ao usuario'});
+  }
+
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(user){
+    request.user = user;
+    return next();
+  }else{
+    return response.status(404).json();
+  }
+
+
 }
 
 app.post('/users', (request, response) => {
